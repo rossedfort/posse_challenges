d8e4939b85a5f240b2837a86d4d14c58d6ca9a57
rossedfort <rossedfort@gmail.com>
2016-02-22 08:51:48 -0700

    add enumerables any, select, reduce, reject

---
+++ enumerables/enums.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Enums
+  def self.any?(collection, condition)
+    collection.each do |num|
+      return true if condition.call(num)
+    end
+    false
+  end
+
+  def self.select(collection, condition)
+    arr = []
+    collection.each do |num|
+      arr.push(num) if condition.call(num)
+    end
+    arr
+  end
+
+  def self.reduce(collection, condition)
+    arr = []
+    collection.each do |element|
+      arr.push(condition.call(element))
+    end
+    arr
+  end
+
+  def self.reject(collection, condition)
+    arr = []
+    collection.each do |num|
+      arr.push(num) unless condition.call(num)
+    end
+    arr
+  end
+end
+
+collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+condition = Proc.new { |a| a % 2 != 0 }
+p Enums.reject(collection, condition)
