498a5cffb3c0afd4e25833f2bccd843fc41db9b3
rossedfort <rossedfort@gmail.com>
2016-03-02 08:57:19 -0700

    getting there

--- turing_tower/lib/turing_tower.rb
+++ turing_tower/lib/turing_tower.rb
@@ -1,4 +1,35 @@
-class TuringTower
-  def initialize
+require 'pry'
+
+class Tower
+  attr_reader :spoke_one, :spoke_two, :spoke_three
+  def initialize(arr1, arr2, arr3)
+    @base_case = arr1
+    @spoke_one = arr1
+    @spoke_two = arr2
+    @spoke_three = arr3
+  end
+
+  def sort
+    if spoke_one.length.even?
+      spoke_two.push(spoke_one.pop)
+    else
+      spoke_three.push(spoke_one.pop)
+    end
+    compare_spokes
+  end
+
+  def compare_spokes
+    until spoke_three == @base_case
+      unless spoke_three.last % 2 == spoke_one.last % 2
+        if spoke_three.empty? || (spoke_one.last < spoke_three.last)
+          spoke_three.push(spoke_one.pop)
+        else
+          spoke_two.push(spoke_one.pop)
+        end
+      end
+    end
+    [spoke_one, spoke_two, spoke_three]
   end
 end
+
+p Tower.new([5,4,3,2,1], [], []).sort
--- turing_tower/test/turing_tower_test.rb
+++ turing_tower/test/turing_tower_test.rb
@@ -4,6 +4,12 @@ require 'minitest/emoji'
 
 class TuringTowerTest < Minitest::Test
   def test_it_exists
-    assert TuringTower.new
+    assert Tower.new([5,4,3,2,1], [], [])
+  end
+
+  def test_it_sorts
+    tower = Tower.new([5,4,3,2,1], [], [])
+
+    assert_equal [[], [], [5,4,3,2,1]], tower.sort
   end
 end
